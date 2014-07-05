cc25c6e68a9ec600e31f0e1beb70ba4890b30e76
Boris Suvorov <bsuvorov@gmail.com>
2014-07-05 16:28:18 -0700

    School/<id> returns list of departments now

--- server/code/ext_api/school/get.py
+++ server/code/ext_api/school/get.py
@@ -2,13 +2,17 @@ from ext_api.exceptions import API_Exception
 from ext_api.http_response_builder import HTTP_Response_Builder, HTTP_Response
 from model.school import School
 from model.db_session import DB_Session_Factory
-
+from model.department import Department
 
 class get_school(HTTP_Response_Builder): 
     def do_controller_specific_work(self):
         db_session = DB_Session_Factory.get_db_session()
-        school = db_session.query(School).get(self.resource_id)
-        if school is None:
+        departments = db_session.query(Department).filter(Department.school_id == self.resource_id)
+        if departments is None:
             raise API_Exception("404 Not Found", "Requested school_id was not found.")
         else:
-          return HTTP_Response('200 OK', {'school' : school.for_api()})
+            departmentCollection = []
+            for department in departments:
+                departmentCollection.append(department.for_api())
+
+            return HTTP_Response('200 OK', {'school_departments' : departmentCollection})
