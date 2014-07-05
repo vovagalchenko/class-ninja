76a7f08a080561fd61b8f990a655fa202f1a8f2f
Boris Suvorov <bsuvorov@gmail.com>
2014-07-05 14:45:55 -0700

    add API to list all schools

--- server/code/ext_api/school/list.py
+++ server/code/ext_api/school/list.py
@@ -1,6 +1,24 @@
+from ext_api.exceptions import API_Exception
 from ext_api.http_response_builder import HTTP_Response_Builder, HTTP_Response
+from model.school import School
+from model.db_session import DB_Session_Factory
 
 class list_school(HTTP_Response_Builder):
-
     def do_controller_specific_work(self):
-        return HTTP_Response('200 OK', {'test_response' : 'blahblah'})
+        db_session = DB_Session_Factory.get_db_session()
+        schools = db_session.query(School).all()
+        if schools is not None:
+            schoolCollection = []
+            for school in schools:
+                schoolJSON = str(school.for_api())
+                schoolCollection.append(schoolJSON)
+            
+            
+            allSchoolsJSON = ",".join(schoolCollection)
+              
+            #schools = School()
+            #schools.__table__.select()
+            
+            return HTTP_Response('200 OK', {'schools' : allSchoolsJSON})
+        else:
+            return HTTP_Response('404 Not found', 'Failed to list schools.')
