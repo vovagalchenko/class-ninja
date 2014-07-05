c51c4da6399c4721f156cf498522c0b8aba46786
Boris Suvorov <bsuvorov@gmail.com>
2014-07-04 22:34:16 -0700

    Wire up department API. 1) create  department package (__init__.py) 2) cleanup bugs in model and get.api

--- server/code/ext_api/department/get.py
+++ server/code/ext_api/department/get.py
@@ -1,7 +1,14 @@
+from ext_api.exceptions import API_Exception
 from ext_api.http_response_builder import HTTP_Response_Builder, HTTP_Response
+from model.department import Department
+from model.db_session import DB_Session_Factory
 
-class get_school(HTTP_Response_Builder):
+class get_department(HTTP_Response_Builder):
+    def do_controller_specific_work(self):  
+        db_session = DB_Session_Factory.get_db_session()
+        department = db_session.query(Department).get(self.resource_id)
+        if department is None:
+            raise API_Exception("404 Not Found", "Requested department_id not found.")
+        else:         
+            return HTTP_Response('200 OK', {'department' : department.for_api()})
 
-    def do_controller_specific_work(self):
-               
-        return HTTP_Response('200 OK', {'departments_list' : 'awesome departments'})
--- server/code/model/department.py
+++ server/code/model/department.py
@@ -1,9 +1,9 @@
-from base import Base
-from sqlalchemy import Column, String, Enum, ForeignKey, Integeer
+from base import Base, Ninja_Model_Mixin
+from sqlalchemy import Column, String, Enum, ForeignKey, Integer
 
 class Department(Base, Ninja_Model_Mixin):
     __tablename__ = 'departments'
     department_id = Column('department_id', String(254), primary_key = True)
-    school_id = Column('department_id', Integer, ForeignKey("schools.school_id"), nullable = False)
+    school_id = Column('school_id', Integer, ForeignKey("schools.school_id"), nullable = False)
     school_specific_id = Column('school_specific_id', String(254), nullable = False)
     name = Column('name', String(254), nullable = False)
