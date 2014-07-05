a8e8f2d08bc67721b2cd2662da67f659e2c2fb55
bsuvorov <bsuvorov@gmail.com>
2014-07-05 16:30:41 -0700

    Add description method to each model

--- ios/ClassNinja/ClassNinja/Classes/CNModels.m
+++ ios/ClassNinja/ClassNinja/Classes/CNModels.m
@@ -8,10 +8,25 @@
 
 #import "CNModels.h"
 @implementation CNSchool
+- (NSString *)description
+{
+    return [NSString stringWithFormat:@"School %@, id = %@, term code = %@, term name = %@", self.name, self.schoolId, self.currentTermCode, self.currentTermName];
+    
+}
 @end
 
 @implementation CNDepartment
+- (NSString *)description
+{
+    return [NSString stringWithFormat:@"Department %@, id = %@, school name = %@", self.name, self.departmentId, self.school.name];
+}
+
 @end
 
 @implementation CNCourse
+- (NSString *)description
+{
+    return [NSString stringWithFormat:@"Course %@, id = %@,  dept spec course id = %@, dept name = %@, ", self.name, self.courseId, self.departmentSpecificCourseId, self.department.name];
+}
+
 @end
