25ba681a60e09da68ef14db4ad9a024fa337ad37
Boris Suvorov <bsuvorov@gmail.com>
2014-07-05 17:10:15 -0700

    Implement department/<id> API to get list of courses for department

--- server/code/ext_api/department/get.py
+++ server/code/ext_api/department/get.py
@@ -1,14 +1,18 @@
 from ext_api.exceptions import API_Exception
 from ext_api.http_response_builder import HTTP_Response_Builder, HTTP_Response
-from model.department import Department
+from model.course import Course
 from model.db_session import DB_Session_Factory
+from model.department import Department
 
-class get_department(HTTP_Response_Builder):
-    def do_controller_specific_work(self):  
+class get_department(HTTP_Response_Builder): 
+    def do_controller_specific_work(self):
         db_session = DB_Session_Factory.get_db_session()
-        department = db_session.query(Department).get(self.resource_id)
-        if department is None:
-            raise API_Exception("404 Not Found", "Requested department_id not found.")
-        else:         
-            return HTTP_Response('200 OK', {'department' : department.for_api()})
+        courses = db_session.query(Course).filter(Course.department_id == self.resource_id)
+        if courses is None:
+            raise API_Exception("404 Not Found", "Requested school_id was not found.")
+        else:
+            coursesCollection = []
+            for course in courses:
+                coursesCollection.append(course.for_api())
 
+            return HTTP_Response('200 OK', {'department_courses' : coursesCollection})
---
+++ server/code/model/course.py
@@ -0,0 +1,11 @@
+from base import Base, Ninja_Model_Mixin
+from sqlalchemy import Column, String, Enum, ForeignKey, Integer
+
+class Course(Base, Ninja_Model_Mixin):
+    __tablename__ = 'courses'
+    course_id = Column('course_id', String(254), primary_key = True)
+    school_id = Column('school_id', Integer, ForeignKey("schools.school_id"), nullable = False)
+    department_id = Column('department_id', String(254), ForeignKey("departments.department_id"), nullable = False)
+    department_specific_course_id = Column('department_specific_course_id', String(254), nullable = False)
+    name = Column('name', String(254), nullable = False)
+    context = Column('context', String(254), nullable = False)
