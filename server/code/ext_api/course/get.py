10805469497ef4facbdbdcd68efc190693019322
Vova Galchenko <vova@galchenko.com>
2014-08-17 15:20:19 -0700

    Propagate the target id to the client for authorized clients

--- server/code/ext_api/course/get.py
+++ server/code/ext_api/course/get.py
@@ -3,6 +3,7 @@ from ext_api.http_response_builder import HTTP_Response_Builder, HTTP_Response
 from model.db_session import DB_Session_Factory
 from model.section import Section
 from model.event import Event
+from model.target import Target
 
 class get_course(HTTP_Response_Builder): 
     def do_controller_specific_work(self):
@@ -23,9 +24,14 @@ class get_course(HTTP_Response_Builder):
 
     def listEventsForSection(self, sectionId): 
         db_session = DB_Session_Factory.get_db_session()
-        events = db_session.query(Event).filter(Event.section_id == sectionId)
+        events = db_session.query(Event).filter(Event.section_id == sectionId).all()
+        targets = {}
+        if self.user is not None:
+            for target in db_session.query(Target).filter(Target.user_phone_number == self.user.phonenumber, Target.event_id.in_(map(lambda event: event.event_id, events))):
+                targets[target.event_id] = target.target_id
         eventsCollection = []
         for event in events:
-            eventsCollection.append(event.for_api())
-
+            event_dict = event.for_api()
+            event_dict['target_id'] = targets.get(event.event_id, None)
+            eventsCollection.append(event_dict)
         return eventsCollection
--- server/code/ext_api/target/list.py
+++ server/code/ext_api/target/list.py
@@ -10,7 +10,11 @@ class list_target(HTTP_Response_Builder):
     def do_controller_specific_work(self):
         if self.user is None:
             raise Authorization_Exception("You must be logged in to list your targets.")
-        events = map(lambda target: target.event, self.user.targets)
+        targets = self.user.targets
+        events = map(lambda target: target.event, targets)
+        targets_by_event_id = {}
+        for target in targets:
+            targets_by_event_id[target.event_id] = target.target_id
         events_by_section_id = dict((k, list(g)) for k, g in groupby(events, lambda event: event.section_id))
         db_session = DB_Session_Factory.get_db_session()
         sections = db_session.query(Section).filter(Section.section_id.in_(events_by_section_id.keys())).all()
@@ -22,7 +26,12 @@ class list_target(HTTP_Response_Builder):
             sections_list = []
             for section in sections_by_course_id.get(course.course_id):
                 section_dict = section.for_api()
-                section_dict['events'] = list(events_by_section_id[section.section_id])
+                events = []
+                for event_in_this_section in events_by_section_id[section.section_id]:
+                    event_dict = event_in_this_section.for_api()
+                    event_dict['target_id'] = targets_by_event_id.get(event_in_this_section.event_id, None)
+                    events.append(event_dict)
+                section_dict['events'] = events
                 sections_list.append(section_dict)
             course_dict['course_sections'] = sections_list
             response.append(course_dict)
