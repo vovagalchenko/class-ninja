3a63a53c395134deee79d1d67336735c6b6ff653
Vova Galchenko <vova@galchenko.com>
2014-09-18 14:23:27 -0700

    Groupby requires the list to be sorted by the criterion

--- server/code/ext_api/target/list.py
+++ server/code/ext_api/target/list.py
@@ -11,13 +11,13 @@ class list_target(HTTP_Response_Builder):
         if self.user is None:
             raise Authorization_Exception("You must be logged in to list your targets.")
         targets = self.user.targets
-        events = map(lambda target: target.event, targets)
+        events = sorted(map(lambda target: target.event, targets), key = lambda event: event.section_id)
         targets_by_event_id = {}
         for target in targets:
             targets_by_event_id[target.event_id] = target.target_id
         events_by_section_id = dict((k, list(g)) for k, g in groupby(events, lambda event: event.section_id))
         db_session = DB_Session_Factory.get_db_session()
-        sections = db_session.query(Section).filter(Section.section_id.in_(events_by_section_id.keys())).all()
+        sections = db_session.query(Section).filter(Section.section_id.in_(events_by_section_id.keys())).order_by(Section.course_id).all()
         sections_by_course_id = dict((k, list(g)) for k, g in groupby(sections, lambda section: section.course_id))
         courses = db_session.query(Course).filter(Course.course_id.in_(sections_by_course_id.keys())).all()
         response = []
