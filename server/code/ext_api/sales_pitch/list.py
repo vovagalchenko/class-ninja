fb2fcf918893854a9d6476d5a529f6f732422c67
Vova Galchenko <vova@galchenko.com>
2014-10-05 01:47:18 -0700

    Added the sales pitch API

---
+++ server/code/ext_api/sales_pitch/list.py
@@ -0,0 +1,6 @@
+from ext_api.exceptions import API_Exception
+from ext_api.http_response_builder import HTTP_Response_Builder, HTTP_Response
+
+class list_sales_pitch(HTTP_Response_Builder):
+    def do_controller_specific_work(self):
+        return HTTP_Response('200 OK', {'sales_pitch' : 'It costs us real money to run this service for you. We hope you enjoy using it.\n\nFor just %@, you will be able to track ten more classes this term.'})
