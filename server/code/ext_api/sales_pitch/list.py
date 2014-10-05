7a02b2963ee7d4e222c5aef0a2c67e545f197e53
Vova Galchenko <vova@galchenko.com>
2014-10-05 10:27:44 -0700

    Fixed sales pitch

--- server/code/ext_api/sales_pitch/list.py
+++ server/code/ext_api/sales_pitch/list.py
@@ -3,4 +3,4 @@ from ext_api.http_response_builder import HTTP_Response_Builder, HTTP_Response
 
 class list_sales_pitch(HTTP_Response_Builder):
     def do_controller_specific_work(self):
-        return HTTP_Response('200 OK', {'sales_pitch' : 'It costs us real money to run this service for you. We hope you enjoy using it.\n\nFor just %@, you will be able to track ten more classes this term.'})
+        return HTTP_Response('200 OK', {'sales_pitch' : 'It costs us real money to run this service for you. We hope you enjoy using it.\n\nFor just %@, you will be able to track ten more classes.'})
