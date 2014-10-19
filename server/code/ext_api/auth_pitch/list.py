d0b7a9f2b84e2db82d1c0b91507ffb357a837558
Vova Galchenko <vova@galchenko.com>
2014-10-19 11:49:36 -0700

    Amended the auth pitch

--- server/code/ext_api/auth_pitch/list.py
+++ server/code/ext_api/auth_pitch/list.py
@@ -3,4 +3,4 @@ from ext_api.http_response_builder import HTTP_Response_Builder, HTTP_Response
 
 class list_auth_pitch(HTTP_Response_Builder):
     def do_controller_specific_work(self):
-        return HTTP_Response('200 OK', {'auth_pitch' : 'Please enter your phone number to register with Class Radar. We will not use your phone number for any other purposes without your authorization.'})
+        return HTTP_Response('200 OK', {'auth_pitch' : 'Please register with Class Radar to start tracking classes you are interested in. You can target 3 classes for free after you register. We don\'t use your phone number for anything other than keeping track of your targets.'})
