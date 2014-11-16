f1afdd5d6515eb87667ccc535a255d558f8c9dee
Boris Suvorov <bsuvorov@gmail.com>
2014-11-16 13:27:29 -0800

    Add information about how many targets we grant for single purchase.

--- server/code/ext_api/sales_pitch/list.py
+++ server/code/ext_api/sales_pitch/list.py
@@ -5,8 +5,9 @@ class list_sales_pitch(HTTP_Response_Builder):
     def do_controller_specific_work(self):
         sales_pitch = {}
         sales_pitch['sales_pitch'] = 'It costs us real money to run this service. We hope you enjoy using it.\n\nFor just %@, you will be able to track another ten classes.'
-        sales_pitch['short_sales_pitch'] = 'Or track 10 more classes for just %@.'
+        sales_pitch['short_sales_pitch'] = 'Or track ten more classes for just %@.'
         sales_pitch['sharing_pitch'] = 'Track another %@ for free by helping us spread the word.'
+        sales_pitch['targets_for_purchase'] = 10
         sales_pitch['targets_for_sharing'] = 3
         sales_pitch['signup_reminder'] = 'We let you track the first %@ classes for free.'
         #if you're changing targest for signup, also change auth_pitch and user/update
