d37224126e45f7fa6fe0b1e728459d2fb62233b3
Boris Suvorov <bsuvorov@gmail.com>
2014-11-23 12:17:27 -0800

    Change wording of "or track x more classes" to use numbers for consistency

--- server/code/ext_api/sales_pitch/list.py
+++ server/code/ext_api/sales_pitch/list.py
@@ -5,7 +5,7 @@ class list_sales_pitch(HTTP_Response_Builder):
     def do_controller_specific_work(self):
         sales_pitch = {}
         sales_pitch['sales_pitch'] = 'It costs us real money to run this service. We hope you enjoy using it.\n\nFor just %@, you will be able to track another ten classes.'
-        sales_pitch['short_sales_pitch'] = 'Or track ten more classes for just %@.'
+        sales_pitch['short_sales_pitch'] = 'Or track 10 more classes for just %@.'
         sales_pitch['sharing_pitch'] = 'Track another %@ for free by helping us spread the word.'
         sales_pitch['targets_for_purchase'] = 10
         sales_pitch['targets_for_sharing'] = 10
